2f19332bd117b050edb519553f28fef71599f23c
bertdev <herbertmatos10@hotmail.com>
2021-05-24 20:58:19 -0300

    refatoraçao de partes do codigo

--- projeto1/src/functions.js
+++ projeto1/src/functions.js
@@ -3,90 +3,86 @@ const path = require("path");
 
 
 function lerDiretorio(caminho) {
-		return new Promise((resolve, reject) => {
-				try {
-						let arquivos = fs.readdirSync(caminho);
-						arquivos = arquivos.map(arquivo => path.join(caminho, arquivo));
-						resolve(arquivos);
-				} catch(e) {
-						reject(e);
-				};
-		});
+	return new Promise((resolve, reject) => {
+		try {
+			const arquivos = fs.readdirSync(caminho).map(arquivo => path.join(caminho, arquivo));
+			resolve(arquivos);
+		} catch(e) {
+			reject(e);
+		};
+	});
  };
 
 function filtrarPorExtensao(extensao) {
-		return function(array) {
-				return array.filter(arquivo => arquivo.endsWith(extensao));
-		};
+	return array => {
+		return array.filter(arquivo => arquivo.endsWith(extensao));
+	};
 };
 
 function lerArquivo(caminho) {
-		return new Promise((resolve, reject) => {
-				try {
-						const conteudo = fs.readFileSync(caminho, {encodin: "utf-8"}); 
-						resolve(conteudo.toString());
-				} catch(e) {
-						reject(e);
-				};
-		});
+	return new Promise((resolve, reject) => {
+		try {
+			const conteudo = fs.readFileSync(caminho, {encodin: "utf-8"}); 
+			resolve(conteudo.toString());
+		} catch(e) {
+			reject(e);
+		};
+	});
 };
 
 function lerArquivos(caminhos) {
-		return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))	
+	return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))	
 };
 
 function removerElementosSeVazio(array) {
-		return array.filter(el => el.trim());
+	return array.filter(el => el.trim());
 };
 
 function removerElementosSeInlcuir(padraoTextual) {
-		return function(array) {
-				return array.filter(el => !el.includes(padraoTextual));
-		};
+	return array => {
+		return array.filter(el => !el.includes(padraoTextual));
+	};
 };
 
 function removerElementosSeApenasNumero(array) {
-		return array.filter(el => el != parseInt(el.trim()));	
+	return array.filter(el => el != parseInt(el.trim()));	
 };
 
 function removerCaracteres(simbolos) {
-		return function(array) {
-				return array.map(el => {
-						return simbolos.reduce((acc, simbolo) => {
-								return acc.split(simbolo).join("");
-						}, el);
-				});
-		};
+	return array => {
+		return array.map(el => {
+			return simbolos.reduce((acc, simbolo) => {
+				return acc.split(simbolo).join("");
+			}, el);
+		});
+	};
 };
 
 function mesclarElementos(elementos) {
-		return elementos.join("");
+	return elementos.join("");
 };
 
 function separarTextoPor(separador) {
-		return texto => texto.split(separador);
+	return texto => texto.split(separador);
 };
 
 function agruparPalavras(palavras) {
-		return Object.values(palavras.reduce((agrupamento, palavra) => {
-				const p = palavra.toLowerCase();
-				const qtde = agrupamento[p] ? agrupamento[p].qtde += 1 : 1;
-				agrupamento[p] = {
-						elemento: p,
-						qtde
-				};
+	return Object.values(palavras.reduce((agrupamento, palavra) => {
+		const p = palavra.toLowerCase();
+		const qtde = agrupamento[p] ? agrupamento[p].qtde += 1 : 1;
+		agrupamento[p] = {
+			elemento: p,
+			qtde
+		};
 
-				return agrupamento;
-		}, {}));
+		return agrupamento;
+	}, {}));
 };
 
 function ordenarPorAtribNumerico(attr, ordem = "asc") {
-		return array => {
-				const asc = (obj1, obj2) => obj1[attr] - obj2[attr];
-				const desc = (obj1, obj2) => obj2[attr] - obj1[attr];
-
-				return array.sort(ordem === "asc" ? asc : desc);
-		};
+	const asc = (obj1, obj2) => obj1[attr] - obj2[attr];
+	const desc = (obj1, obj2) => obj2[attr] - obj1[attr];
+	return array => array.sort(ordem === "asc" ? asc : desc);
 };
 
 module.exports = {
--- projeto1/src/index.js
+++ projeto1/src/index.js
@@ -4,25 +4,26 @@ const path = require("path");
 const caminho = path.join(__dirname,"..", "legendas");
 
 const simbolos = [
-		"!", "-", "?", '"', ".", ",", "♪",
-		"_", "<i>", "</i>", "\r", "[", "]",
-		"{", "}", "(", ")"
+	"!", "-", "?", '"', ".", ",", "♪",
+	"_", "<i>", "</i>", "\r", "[", "]",
+	"{", "}", "(", ")"
 ];
 
 fun.lerDiretorio(caminho)
-		.then(fun.filtrarPorExtensao(".srt"))
-		.then(fun.lerArquivos)
-		.then(fun.mesclarElementos)
-		.then(fun.separarTextoPor("\n")) 
-		.then(fun.removerElementosSeVazio)
-		.then(fun.removerElementosSeInlcuir("-->"))
-		.then(fun.removerElementosSeApenasNumero)
-		.then(fun.removerCaracteres(simbolos))
-		.then(fun.mesclarElementos)
-		.then(fun.separarTextoPor(" "))
-		.then(fun.removerElementosSeVazio)
-		.then(fun.removerElementosSeApenasNumero)
-		.then(fun.agruparPalavras)
-		.then(fun.ordenarPorAtribNumerico("qtde", "desc"))
-		.then(console.log);
+	.then(fun.filtrarPorExtensao(".srt"))
+	.then(fun.lerArquivos)
+	.then(fun.mesclarElementos)
+	.then(fun.separarTextoPor("\n")) 
+	.then(fun.removerElementosSeVazio)
+	.then(fun.removerElementosSeInlcuir("-->"))
+	.then(fun.removerElementosSeApenasNumero)
+	.then(fun.removerCaracteres(simbolos))
+	.then(fun.removerElementosSeInlcuir("<font>"))
+	.then(fun.mesclarElementos)
+	.then(fun.separarTextoPor(" "))
+	.then(fun.removerElementosSeVazio)
+	.then(fun.removerElementosSeApenasNumero)
+	.then(fun.agruparPalavras)
+	.then(fun.ordenarPorAtribNumerico("qtde", "desc"))
+	.then(console.log);
 
